0b7bc3154c2d54482fbdc5bad3f0a553843cd7b3
danbirle <razvan.birle@gmail.com>
2021-07-23 21:28:07 +0200

    add scss support

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,10 +4,12 @@
 
 <main>
 	<h1>Hello {name}!</h1>
+	<input type="text" placeholder="name" bind:value="{name}" />
+	<button>Start</button>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
